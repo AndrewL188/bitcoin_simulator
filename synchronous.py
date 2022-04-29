d5b61a5cc3017c00f491943ebfc9d933236fa66d
Andrew Luo <ajluo2@illinois.edu>
2022-04-28 22:19:17 -0500

    Finished synchronous 0 delay simulations and theoretical prob

--- synchronous.py
+++ synchronous.py
@@ -1,5 +1,32 @@
 import random
 import math
+import numpy as np
+
+def build_lead_simulation(q) -> int:
+    private_lead = 0
+    target_t_time = random.randint(50,100)
+    for i in range(target_t_time):
+        if (nextBlockMalicious(q)):
+            private_lead += 1
+        else:
+            private_lead = max(private_lead-1, 0)
+    return private_lead
+
+def lead_distribution(q, num_iter=100000):
+    distribution = dict()
+    for i in range(num_iter):
+        j = build_lead_simulation(q)
+        distribution[j] = distribution.get(j, 0) + 1
+    
+    for i in range(10):
+        if i in distribution:
+            p_i = distribution[i]/num_iter
+            print("Probability of " + str(i) + ": " + str(p_i))
+    
+    # x = np.arange(0, len(distribution), 1)
+    # y = np.array([distribution[i]/num_iter for i in range(len(distribution))])
+    # fit = np.polyfit(x, np.log(y), 1)
+    # print(fit)
 
 def best_attack_simulation(q, z) -> bool:
     private_lead = 0
@@ -66,14 +93,42 @@ def computeAttackerSuccessProb(q, z):
         p_numAblocks = math.comb(2*z, j)*(q**j)*(p**(2*z-j))
         pwin += pwin_numAblocks*p_numAblocks
     return pwin
+def computeBestAttackSuccessProb(q, n):
+    p = 1-q
+    p_win = 0
+    for i in range(n):
+        p_y_i = (1-q/p)*((q/p)**i)
+        p_win_y_i = 0
+        for j in range(2*n - i + 1):
+            p_w_xj_yi = 1
+            p_xj_yi = math.comb(2*n-i, j)*(q**j)*(p**(2*n-i-j))
+            if (j < n - i):
+                p_w_xj_yi = (q/p)**(2*n-2*i-2*j)
+            p_win_y_i += p_w_xj_yi*p_xj_yi
+        p_win += p_win_y_i*p_y_i
+    p_win += (q/p)**n
+    return p_win
+        
+
 
 def main():
-    print("Computed attacker success: " + str(computeAttackerSuccessProb(0.2, 6)))
-    print("Nakamoto attack empirical attacker success: " + str(computeEmpiricalSuccess(nakamoto_malicious_simulation, 0.2, 6, 10000)))
-    print("Best attack empirical attacker success: " + str(computeEmpiricalSuccess(best_attack_simulation, 0.2, 6, 10000)))
-
+    #print("Computed attacker success: " + str(computeAttackerSuccessProb(0.05, 6)))
+    #print("Nakamoto attack empirical attacker success: " + str(computeEmpiricalSuccess(nakamoto_malicious_simulation, 0.2, 6, 10000)))
+    #print("Best attack empirical attacker success: " + str(computeEmpiricalSuccess(best_attack_simulation, 0.2, 6, 100000)))
+    #print("Best attack computed attacker success: " + str(computeBestAttackSuccessProb(0.2, 6)))
+    # lead_distribution(0.02)
     
-  
+    delta = 0.05
+    print("         Empirical success Prob | Theoretical Prob")
+    for i in range(2,9):
+        q = delta*i
+        best_emp_p = computeEmpiricalSuccess(best_attack_simulation, q, 6, 30003)
+        best_comp_p = computeBestAttackSuccessProb(q, 6)
+        space = "   "
+        if i%2==1:
+            space = "  "
+        print("q: " + str(round(q,2)) + space + str(round(best_emp_p,8)) + "               " + str(round(best_comp_p, 8)))
+    
 # Using the special variable 
 # __name__
 if __name__=="__main__":
